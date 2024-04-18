ee2945bab152d0bb62194bcb697d8ceaaa0971e6
Gregory Soares <gregory16704@gmail.com>
2024-04-18 09:22:41 -0300

    foi

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    botaoMostrarProjetos.classList.add('remover');
+});
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+};
