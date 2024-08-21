4ed7219d18600af80a46372e703915a59af518b7
Gregory Soares <gregory16704@gmail.com>
2024-08-21 12:06:45 -0300

    Adicionando botão que oculta os outros projetos

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+const botaoOcultarProjetos = document.querySelector('.btn-ocultar-projetos');
+const projetos = document.querySelectorAll('.projeto');
 
 botaoMostrarProjetos.addEventListener('click', () => {
     mostrarMaisProjetos();
     botaoMostrarProjetos.classList.add('remover');
+    botaoOcultarProjetos.classList.remove('remover')
 });
 
 function mostrarMaisProjetos() {
@@ -11,3 +14,19 @@ function mostrarMaisProjetos() {
         projetoInativo.classList.add('ativo');
     });
 };
+botaoOcultarProjetos.addEventListener('click', () => {
+    ocultarProjetos();
+    botaoMostrarProjetos.classList.remove('remover');
+    botaoOcultarProjetos.classList.add('remover')
+});
+
+function ocultarProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.remove('ativo');
+    });
+};
+// function ocultarProjetos() {
+//     projetos.forEach(projeto => {
+//         projeto.classList.remove('ativo');
+//     });
+// };
