62735e084ec17b4394000fa5f6dea1311149a969
Gregory Soares <gregory16704@gmail.com>
2025-05-16 14:39:41 -0300

    feat(certificates): permitindo visualizar o certificado inteiro ao clicar

--- src/js/certificates.js
+++ src/js/certificates.js
@@ -2,24 +2,33 @@ document.addEventListener("DOMContentLoaded", async (event) =>{
     fetch("../json/certificates.json").then(data => data.json()).then(data => {criarCertificados(data)});
 });
 
-function filtra(skill) {
+const filtersArray = [];
+const technologies = document.querySelectorAll(".technologies");
+let imgs = document.querySelectorAll(".img-certificado");
+
+
+function filtrar(skill) {
+    if(filtersArray.includes(skill)){
+        const index = filtersArray.indexOf(skill);
+        filtersArray.splice(index,1);
+    } 
+    else filtersArray.push(skill);
+    
     fetch("../json/certificates.json").
     then(data => data.json())
     .then(certificates => {
         const newCertificates = certificates.filter(certificate => 
-            certificate.skills.includes(skill)
+            filtersArray.every(element => certificate.skills.includes(element))
         );
-        console.log(newCertificates);
-        
         esconderCertificados(newCertificates);
     });
+    imgs = document.querySelectorAll(".img-certificado");
 }
 
 function esconderCertificados(certificados) {
     const section = document.querySelectorAll('.certificado');
     section.forEach(img => {
-        console.log(img.firstChild)
-        img.classList.toggle('hide');
+        img.classList.add('hide');
         certificados.forEach(certificado => {
             if(img.firstChild.getAttribute('alt').match(certificado.title)) {
                 img.classList.toggle('hide');
@@ -27,7 +36,6 @@ function esconderCertificados(certificados) {
             };
         });
     });
-
 }
 
 function criarCertificados(certificados) {
@@ -40,7 +48,43 @@ function criarCertificados(certificados) {
         img.className = 'img-certificado';
         img.src=certificado.image;
         img.alt=certificado.title;
+        img.addEventListener("click", (e) => mostrarImg(e));
         div.appendChild(img);
     });
-    
+    imgs = document.querySelectorAll(".img-certificado");
+    console.log(imgs);
+
 }
+  
+technologies.forEach(btn => btn.addEventListener("click", (e) => {
+    btn.classList.toggle("selected");
+}))
+
+function mostrarImg(e) {
+    // imgs.forEach(img => img.addEventListener("click", (e) => {
+    //     console.log(e.target);
+    //     console.log(img);
+        console.log("foi");
+        console.log(e.target)
+        
+        const div = document.createElement('div');
+        console.log(div);
+
+        document.body.appendChild(div);
+        div.className="modal";
+        const bigImg = document.createElement('img');
+        div.appendChild(bigImg);
+        bigImg.className="modal-content";
+        bigImg.src=e.target.src;
+        
+        window.addEventListener("click", (a)=>{  
+            if(a.target==div || a.target==bigImg)  {
+            console.log(document.querySelector(".modal"));
+            console.log(a.target);
+            console.log(bigImg);
+            if(a.target!=bigImg) div.remove();
+            if(a.target!=bigImg) console.log("é diferente");
+        }});
+    // }))
+}
+    
